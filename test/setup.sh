830ec3a1a4d2ccb1f2c9b8e6e711ae7e67827e3a
Christopher Haar <christopher.haar@upbound.io>
2024-02-25 14:42:52 +0100

    feat(caas): use configurations in caas (#17)
    Signed-off-by: Christopher Haar <christopher.haar@upbound.io>

--- test/setup.sh
+++ test/setup.sh
@@ -4,61 +4,82 @@ set -aeuo pipefail
 UPTEST_GCP_PROJECT=${UPTEST_GCP_PROJECT:-official-provider-testing}
 
 echo "Running setup.sh"
-echo "Waiting until configuration package is installed..."
-${KUBECTL} wait configuration.pkg configuration-caas --for=condition=Installed --timeout 10m
-echo "Waiting until configuration package is healthy..."
-${KUBECTL} wait configuration.pkg configuration-caas --for=condition=Healthy --timeout 10m
+echo "Waiting until all configuration packages are healthy/installed..."
+"${KUBECTL}" wait configuration.pkg --all --for=condition=Healthy --timeout 10m
+"${KUBECTL}" wait configuration.pkg --all --for=condition=Installed --timeout 10m
+"${KUBECTL}" wait configurationrevisions.pkg --all --for=condition=Healthy --timeout 10m
 
-echo "Creating aws cloud credential secret..."
-${KUBECTL} -n upbound-system create secret generic aws-creds --from-literal=credentials="${UPTEST_AWS_CLOUD_CREDENTIALS}" \
-    --dry-run=client -o yaml | ${KUBECTL} apply -f -
-
-echo "Creating azure cloud credential secret..."
-${KUBECTL} -n upbound-system create secret generic azure-creds --from-literal=credentials="${UPTEST_AZURE_CLOUD_CREDENTIALS}" \
-    --dry-run=client -o yaml | ${KUBECTL} apply -f -
-
-echo "Creating gcp cloud credential secret..."
-${KUBECTL} -n upbound-system create secret generic gcp-creds --from-literal=credentials="${UPTEST_GCP_CLOUD_CREDENTIALS}" \
-    --dry-run=client -o yaml | ${KUBECTL} apply -f -
+echo "Waiting until all installed provider packages are healthy..."
+"${KUBECTL}" wait provider.pkg --all --for condition=Healthy --timeout 10m
 
 echo "Waiting for all pods to come online..."
-"${KUBECTL}" -n upbound-system wait --for=condition=Available deployment --all --timeout=10m
+"${KUBECTL}" -n upbound-system wait --for=condition=Available deployment --all --timeout=5m
 
 echo "Waiting for all XRDs to be established..."
 "${KUBECTL}" wait xrd --all --for condition=Established
 
-echo "Creating a default aws providerconfig..."
-cat <<EOF | ${KUBECTL} apply -f -
+if [[ -n "${UPTEST_CLOUD_CREDENTIALS:-}" ]]; then
+  # UPTEST_CLOUD_CREDENTIALS may contain more than one cloud credentials that we expect to be provided
+  # in a single GitHub secret. We expect them provided as key=value pairs separated by newlines. Currently we expect
+  # two AWS IAM user credentials to be provided. For example:
+  # AWS='[default]
+  # aws_access_key_id = REDACTED
+  # aws_secret_access_key = REDACTED'
+  # AZURE='{
+  # "clientId": "REDACTED",
+  # "clientSecret": "REDACTED",
+  # "subscriptionId": "REDACTED",
+  # "tenantId": "REDACTED",
+  # "activeDirectoryEndpointUrl": "https://login.microsoftonline.com",
+  # "resourceManagerEndpointUrl": "https://management.azure.com/",
+  # "activeDirectoryGraphResourceId": "https://graph.windows.net/",
+  # "sqlManagementEndpointUrl": "https://management.core.windows.net:8443/",
+  # "galleryEndpointUrl": "https://gallery.azure.com/",
+  # "managementEndpointUrl": "https://management.core.windows.net/"
+  # }'
+  # GCP='
+  # {
+  # "type": "service_account",
+  # "project_id": "caramel-goat-354919",
+  # "private_key_id": "e97e40a4a27661f12345678f4bd92139324dbf46",
+  # "private_key": "-----BEGIN PRIVATE KEY-----\n===\n-----END PRIVATE KEY-----\n",
+  # "client_email": "my-sa-313@caramel-goat-354919.iam.gserviceaccount.com",
+  # "client_id": "103735491955093092925",
+  # "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  # "token_uri": "https://oauth2.googleapis.com/token",
+  # "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  # "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/my-sa-313%40caramel-goat-354919.iam.gserviceaccount.com"
+  # }'
+  # UPBOUND='xyz'
+
+  eval "${UPTEST_CLOUD_CREDENTIALS}"
+
+  if [[ -n "${AWS:-}" ]]; then
+    echo "Creating the AWS default cloud credentials secret..."
+    ${KUBECTL} -n upbound-system create secret generic aws-creds --from-literal=credentials="${AWS}" --dry-run=client -o yaml | ${KUBECTL} apply -f -
+
+    echo "Creating the AWS default provider config..."
+    cat <<EOF | ${KUBECTL} apply -f -
 apiVersion: aws.upbound.io/v1beta1
 kind: ProviderConfig
 metadata:
   name: default
 spec:
   credentials:
+    source: Secret
     secretRef:
-      key: credentials
       name: aws-creds
       namespace: upbound-system
-    source: Secret
-EOF
-
-echo "Creating a default azure providerconfig..."
-cat <<EOF | ${KUBECTL} apply -f -
-apiVersion: azure.upbound.io/v1beta1
-kind: ProviderConfig
-metadata:
-  name: default
-spec:
-  credentials:
-    secretRef:
       key: credentials
-      name: azure-creds
-      namespace: upbound-system
-    source: Secret
 EOF
+  fi
 
-echo "Creating a default gcp providerconfig..."
-cat <<EOF | ${KUBECTL} apply -f -
+  if [[ -n "${GCP:-}" ]]; then
+    echo "Creating the GCP default cloud credentials secret..."
+    ${KUBECTL} -n upbound-system create secret generic gcp-creds --from-literal=credentials="${GCP}" --dry-run=client -o yaml | ${KUBECTL} apply -f -
+
+    echo "Creating the GCP default provider config..."
+    cat <<EOF | ${KUBECTL} apply -f -
 apiVersion: gcp.upbound.io/v1beta1
 kind: ProviderConfig
 metadata:
@@ -72,3 +93,46 @@ spec:
     source: Secret
   projectID: ${UPTEST_GCP_PROJECT}
 EOF
+  fi
+
+  if [[ -n "${UPBOUND:-}" ]]; then
+    echo "Creating the upbound-saas default credentials secret..."
+    ${KUBECTL} -n upbound-system create secret generic upbound-creds --from-literal=credentials="${UPBOUND}" --dry-run=client -o yaml | ${KUBECTL} apply -f -
+
+    echo "Creating the upbound-saas default provider config..."
+    cat <<EOF | ${KUBECTL} apply -f -
+apiVersion: upbound.io/v1alpha1
+kind: ProviderConfig
+metadata:
+  name: default
+spec:
+  organization: upbound
+  credentials:
+    secretRef:
+      key: credentials
+      name: upbound-creds
+      namespace: upbound-system
+    source: Secret
+EOF
+  fi
+
+  if [[ -n "${AZURE:-}" ]]; then
+    echo "Creating the AZURE default cloud credentials secret..."
+    ${KUBECTL} -n upbound-system create secret generic azure-creds --from-literal=credentials="${AZURE}" --dry-run=client -o yaml | ${KUBECTL} apply -f -
+
+    echo "Creating the Azure default provider config..."
+    cat <<EOF | ${KUBECTL} apply -f -
+apiVersion: azure.upbound.io/v1beta1
+kind: ProviderConfig
+metadata:
+  name: default
+spec:
+  credentials:
+    source: Secret
+    secretRef:
+      name: azure-creds
+      namespace: upbound-system
+      key: credentials
+EOF
+  fi
+fi
